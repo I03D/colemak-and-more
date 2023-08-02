9889f8742d6ee09ebdcc593a1ff84cb8e72a6e5c
I03D <98839825+I03D@users.noreply.github.com>
2023-08-03 00:12:18 +0700

    Add files via upload

--- browse.py
+++ browse.py
@@ -1,3 +1,3 @@
 import os
 prompt = input("Введите запрос: ")
-os.system("chrome.exe.lnk google.com/search?q="+prompt.replace(" ", "+"))
+os.system("chrome.exe.lnk --new-window google.com/search?q="+prompt.replace(" ", "+"))
