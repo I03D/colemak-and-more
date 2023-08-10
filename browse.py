c8bf480d66104b5b6ed8cf9ea84fecf47512d292
I03D <98839825+I03D@users.noreply.github.com>
2023-08-10 20:53:20 +0700

    Add files via upload

--- browse.py
+++ browse.py
@@ -1,3 +1,4 @@
+# Этот скрипт предполагает наличие ярлыка Chrome - chrome.exe (chrome.exe.lnk)
 import os
 prompt = input("Введите запрос: ")
 os.system("chrome.exe.lnk --new-window google.com/search?q="+prompt.replace(" ", "+"))
